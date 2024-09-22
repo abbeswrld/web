5e92c24198fc23acbf50901f99299c43322d04d8
zxcpisya <mamin.papa0@gmail.com>
2024-09-22 18:44:19 +1000

    handle exceptions with server hostname and port, added thread and

--- src/scripts/server_slon.py
+++ src/scripts/server_slon.py
@@ -1,21 +1,29 @@
 import socket
+import threading
 
 
 class Server:
-    def __init__(self, hostname="mamin_papa_ded", port=1234):
+    def __init__(self, hostname, port):
         self.__socket = socket.socket()
-        self.__hostname = hostname
-        self.__port = port
+        self.__hostname = hostname if hostname else "mamin_papa_ded"
+        self.__port = port if port else 1234
         self.__connection = None
-        self.__client_address = None
+        self.__client_addresses = []
+        self.__thread = None
 
-    def start_server_listen(self):
+    def start_server_thread(self):
+        self.__thread = threading.Thread(target=self.__start_listen)
+        self.__thread.start()
+
+    def __start_listen(self):
         try:
             self.__socket.bind((self.__hostname, self.__port))
             self.__socket.listen(1)
-            self.__connection, self.__client_address = self.__socket.accept()
-            return "successfully"
+            self.__connection, current_address = self.__socket.accept()
+            while self.__connection:
+                self.handle_message_from_client()
         except Exception as e:
+            print(str(e))
             return str(e)
 
     def handle_message_from_client(self):
@@ -23,13 +31,9 @@ class Server:
             data = self.__connection.recv(1024)
             if not data:
                 break
-            print(data.decode())
+            if data.decode() == "change_image":
+
 
     def close_connection_with_client(self):
         self.__connection.close()
         self.__connection = None
-
-
-s = Server()
-s.start_server_listen()
-s.handle_message_from_client()
