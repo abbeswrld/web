aaf204026d7075e6a8405c18d75fdbcd3039305b
zxcpisya <mamin.papa0@gmail.com>
2024-09-22 18:41:09 +1000

    handle exception with connect to server

--- src/scripts/client_slon.py
+++ src/scripts/client_slon.py
@@ -6,13 +6,12 @@ class Client:
 		self.__socket = socket.socket()
 
 	def connect_to_server(self, hostname="mamin_papa_ded", port=1234):
-		self.__socket.connect((hostname, port))
+		try:
+			self.__socket.connect((hostname, port))
+			return hostname, port
+		except Exception as e:
+			return str(e)
 
 	def send_message_to_server(self, message):
 		message = message.encode() if type(message) is str else message
 		print(self.__socket.send(message))
-
-
-client = Client()
-client.connect_to_server()
-client.send_message_to_server("123")
