6ccba42a5f427f9f6103472f8bf67d45f1e80330
zxcpisya <mamin.papa0@gmail.com>
2024-09-22 18:40:22 +1000

    delete gameWindow.py

--- src/scripts/gameWindow.py
+++
@@ -1,8 +0,0 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow
-
-
-class GameWindow(QMainWindow):
-	def __init__(self):
-		super().__init__()
-		uic.loadUi("src/resources/UI/game.ui", self)
