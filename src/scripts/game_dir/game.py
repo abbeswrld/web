fcb35c4c5da34a6fe696821d1f1d4f2c10718cc9
zxcpisya <mamin.papa0@gmail.com>
2024-09-26 22:12:41 +1000

    added funcs - check_win, reset

--- src/scripts/game_dir/game.py
+++ src/scripts/game_dir/game.py
@@ -1,16 +1,28 @@
 class Game:
-	word: str
-	available_letters = [chr(i) for i in range(ord("а"), ord("а") + 32)]
-	used_letters: []
-	players_turn = False
-	is_game_ended = False
+	word: list
+	bool_word: list
+	players_turn: bool  # False - server's turn, True - client's turn.
+	available_letters: list
+	used_letters: list
 
 	def __init__(self, word):
-		self.word = word
+		self.word = list(word)
+		self.reset()
 
 	def on_letter_use(self, letter: str):
 		self.available_letters.remove(letter)
 		self.used_letters.append(letter)
 
-	def change_players_turn(self):
+	def change_players_turn(self) -> bool:
 		self.players_turn = not self.players_turn
+		return self.players_turn
+
+	def check_win(self) -> bool:
+		return all(self.bool_word)
+
+	def reset(self):
+		self.word = []
+		self.bool_word = [False] * 5
+		self.players_turn = False
+		self.available_letters = [chr(i) for i in range(ord("а"), ord("а") + 32)]
+		self.used_letters = []
