98989a86c3fbcaed177ad76ebee253099086e658
zxcpisya <mamin.papa0@gmail.com>
2024-09-26 16:22:48 +1000

    refactor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QApplication
 import sys
-from src.scripts.mainWindow import MainWindow
+from src.scripts.UI.mainWindow import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
