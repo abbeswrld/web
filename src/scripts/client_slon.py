5ea32b9cb0288dad833cf8ab00115d3fb72a4136
zxcpisya <mamin.papa0@gmail.com>
2024-09-29 12:47:43 +1000

    handle exceptions

--- src/scripts/client_slon.py
+++ src/scripts/client_slon.py
@@ -3,32 +3,30 @@ import time
 
 from src.scripts.UI.gameWindow import GameWindow
 from src.scripts.game_dir.gameCoordinator import ClientGameCoordinator
+from useful_func import create_and_start_thread
 
 
 class Client:
 	def __init__(self, client_UI):
 		self.__client_UI = client_UI
 		self.__socket = socket.socket()
-		self.__client_UI.button_confirm.clicked.connect(self.connect_to_server)
-
-	def connect_to_server(self):
-		hostname = self.__client_UI.get_entered_hostname()
-		port = self.__client_UI.get_entered_port()
-		hostname = hostname if hostname else "mamin_papa_ded"
 		try:
-			self.__socket.connect((hostname, port))
-			time.sleep(0.5)
-			self.on_connect()
-			self.__client_UI.close()
-			return True
+			self.__client_UI.button_confirm.clicked.connect(self.connect_to_server)
 		except Exception as e:
 			print(e)
-			return False
+
+	def connect_to_server(self):
+		try:
+			hostname = self.__client_UI.get_entered_hostname()
+			port = self.__client_UI.get_entered_port()
+			hostname = hostname if hostname else "mamin_papa_ded"
+			self.__socket.connect((hostname, port))
+			self.on_connect()
+		except Exception as e:
+			print(e)
 
 	def on_connect(self):
-		gw = GameWindow()
-		gw.show()
-		ClientGameCoordinator(self, gw)
+		ClientGameCoordinator(self)
 		self.__client_UI.destroy()
 
 	def send_message_to_server(self, msg):
